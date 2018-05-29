35f64d801f971bb50710bfa54e61c4b9f4b71a55
barbarayachnin2018 <39123964+barbarayachnin2018@users.noreply.github.com>
2018-05-29 14:12:38 -0400

    Final push
    Add all changes for final project.

---
+++ js/topNav.js
@@ -0,0 +1,8 @@
+function topNav() {
+    var x = document.getElementById("mobilNav");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
